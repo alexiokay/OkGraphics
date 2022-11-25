325b67d8e3d73c6b3461d741c17424b764fbb4fb
papadi166 <papadi166@gmail.com>
2022-11-25 02:06:45 +0100

    logo path fixed

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
 div#navbar-wrapper(class=" w-full h-[7rem] flex items-center justify-between lg:justify-center mx-0 bg-[#202020]") 
   div#navbar-content(class=" w-full lg:w-[87%] h-full flex items-center justify-center self-start mx-0")
     div#navbar-image(class=" w-[90px]   h-[97px] flex items-center justify-start lg:justify-center mx-0")
-      embed#logo( src="/assets/images/Logo-cropped.svg" alt="logo")
+      embed#logo( :src="`../assets/images/Logo-cropped.svg`" alt="logo")
     div#navbar-empty(class=" w-[0px] lg:w-[22%] h-full flex items-center justify-center mx-0")
     div#navbar-content(class="  lg:w-[44%] w-[66%] h-full flex items-center justify-around mx-0 tracking-wider text-white font-brown  font-normal ")
       p(class="navbar-text hidden sm:block hover:text-blue-700 "  @click='$router.push({name: "index"})') O MNIE
