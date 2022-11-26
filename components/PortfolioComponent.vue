f58c04db4024f0802f0a784e6530652ca22cba80
papadi166 <papadi166@gmail.com>
2022-11-26 17:35:48 +0100

    added widh and height to img for seo

--- components/PortfolioComponent.vue
+++ components/PortfolioComponent.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
  
 div(class="w-full md:w-1/3 lg:w-1/5 bg-black relative flex items-center justify-center")
-    img(class=" " :src="props.image" v-bind:alt="props.alt? props.alt : 'image'" )
+    img(class=" " :src="props.image" v-bind:alt="props.alt? props.alt : 'image'" width="" height="" )
     div(class="absolute flex items-center justify-center bg-black w-full h-full opacity-0 hover:opacity-100 transition ease-in-out delay-50")
         div(class="flex flex-col h-1/3 items-center justify-around")
             p(class="text-white text-center") Project name 2021 
--- components/WorkComponent.vue
+++ components/WorkComponent.vue
@@ -2,7 +2,7 @@
  
 
 div(class=" w-[29%] relative flex flex-col items-center justify-center ")
-    img(class=" h-52 w-full object-cover hover:cursor-pointer " :src="props.image  "  v-bind:alt="props.alt? props.alt : props.title"  )
+    img(class=" h-52 w-full object-cover hover:cursor-pointer " width='100%' height='' :src="props.image  "  v-bind:alt="props.alt? props.alt : props.title"  )
     p(class="text-white text-center text-base font-adelia mt-4") Przyciagnij wzrok        
     p(class="text-[#ffee32] text-center text-2xl mt-2 font-semibold  hover:cursor-pointer ") {{props.title}}
     p(class="text-white text-center mt-4 text-lg ") o jak się prezentujesz, wpływana pierwsze wrażenie. Ludzie w świecie biznesu mają tylko chwilę żeby Ciebie zauważyć. Czy Twoje Logodobrze się prezentuje?
