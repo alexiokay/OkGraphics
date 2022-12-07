cb5cf357241f69262b9b410eb0aaa56eddd01811
papadi166 <papadi166@gmail.com>
2022-12-07 18:34:51 +0100

    .

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,7 @@ div(class=" w-full h-screen  flex flex-col  bg-[#CFB53B]")
         div(class="flex h-full items-start p-10 flex-col w-full sm:w-[96%] xl:w-[87%] justify-center rounded-md bg-[#000000]")    
             div(class="w-full flex items-center flex-col md:flex-row")
                     div(class="flex w-4/5 md:w-2/4  justify-center items-center")
-                        img(src="/images/avatar2.webp" class=" w-5/6 h-full sm:w-2/4 md:w-3/4 md:h-5/6 lg:w-2/4 lg:h-full" width="" height="" alt="avatar")
+                        img(src="/images/Avatar2.webp" class=" w-5/6 h-full sm:w-2/4 md:w-3/4 md:h-5/6 lg:w-2/4 lg:h-full" width="" height="" alt="avatar")
                     div(class="flex flex-col justify-start w-full md:w-2/4 space-y-4 mt-4 ")
                         h1(class=" text-4xl text-center text-[#CFB53B] md:text-start") O mnie
                         p(class="text-white text-center md:text-start") Nazywam się Oktawia Kaczmarska, jestem grafikiem komputerowym. Doświadczenie zdobyłam poprzez samokształcenie się zdalnie i nadal kreuje swoje umiejętności, poszerzając przy tym swoje portfolio. Zrealizowałam już kilka projektów graficznych, odnosząc przy tym sukces zawodowy z zadowolonych klientów.
