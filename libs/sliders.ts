b1a1982e933d9073786ad6f517a4933e81cf02fc
papadi166 <papadi166@gmail.com>
2022-12-19 13:08:57 +0100

    test

--- libs/sliders.ts
+++ libs/sliders.ts
@@ -1,58 +1,58 @@
 export const wizytowki = [
   {
     name: "Slide 1",
-    img: "images/wizytowki/Wizytówka-Informatyk.webp",
+    img: "/images/wizytowki/Wizytówka-Informatyk.webp",
   },
   {
     name: "Slide 2",
-    img: "images/wizytowki/Wizytówka-Adwokat.webp",
+    img: "/images/wizytowki/Wizytówka-Adwokat.webp",
   },
   {
     name: "Slide 3",
-    img: "images/wizytowki/Wizytówka-Adwokatx2.webp",
+    img: "/images/wizytowki/Wizytówka-Adwokatx2.webp",
   },
   {
     name: "Slide 4",
-    img: "images/wizytowki/Wizytówka-Elektryk.webp",
+    img: "/images/wizytowki/Wizytówka-Elektryk.webp",
   },
   {
     name: "Slide 5",
-    img: "images/wizytowki/Wizytówka-Makijaz.webp",
+    img: "/images/wizytowki/Wizytówka-Makijaz.webp",
   },
   {
     name: "Slide 6",
-    img: "images/wizytowki/Wizytówka-Mechanik.webp",
+    img: "/images/wizytowki/Wizytówka-Mechanik.webp",
   },
   {
     name: "Slide 7",
-    img: "images/wizytowki/Wizytówka-Nieruchomosci.webp",
+    img: "/images/wizytowki/Wizytówka-Nieruchomosci.webp",
   },
   {
     name: "Slide 8",
-    img: "images/wizytowki/Wizytówka-Ortodonta.webp",
+    img: "/images/wizytowki/Wizytówka-Ortodonta.webp",
   },
   {
     name: "Slide 9",
-    img: "images/wizytowki/Wizytówka-Psycholog.webp",
+    img: "/images/wizytowki/Wizytówka-Psycholog.webp",
   },
   {
     name: "Slide 10",
-    img: "images/wizytowki/Wizytówka-Salonfryzjerski.webp",
+    img: "/images/wizytowki/Wizytówka-Salonfryzjerski.webp",
   },
   {
     name: "Slide 10",
-    img: "images/wizytowki/zdjecie-glowne.webp",
+    img: "/images/wizytowki/zdjecie-glowne.webp",
   },
 ];
 
 export const ulotki = [
   {
     name: "Slide 1",
-    img: "images/header-img.jpg",
+    img: "/images/header-img.jpg",
   },
   {
     name: "Slide 2",
-    img: "images/wizytowki/Wizytówka-Informatyk.webp",
+    img: "/images/wizytowki/Wizytówka-Informatyk.webp",
   },
   {
     name: "Slide 3",
@@ -71,15 +71,15 @@ export const loga = [
   },
   {
     name: "Slide 2",
-    img: "images/loga/2.webp",
+    img: "/images/loga/2.webp",
   },
   {
     name: "Slide 3",
-    img: "images/loga/3.webp",
+    img: "/images/loga/3.webp",
   },
   {
     name: "Slide 4",
-    img: "images/loga/4.webp",
+    img: "/images/loga/4.webp",
   },
   {
     name: "Slide 5",
@@ -95,291 +95,291 @@ export const loga = [
   },
   {
     name: "Slide 8",
-    img: "images/loga/8.webp",
+    img: "/images/loga/8.webp",
   },
   {
     name: "Slide 9",
-    img: "images/loga/9.webp",
+    img: "/images/loga/9.webp",
   },
   {
     name: "Slide 10",
-    img: "images/loga/10.webp",
+    img: "/images/loga/10.webp",
   },
   {
     name: "Slide 11",
-    img: "images/loga/11.webp",
+    img: "/images/loga/11.webp",
   },
   {
     name: "Slide 12",
-    img: "images/loga/12.webp",
+    img: "/images/loga/12.webp",
   },
   {
     name: "Slide 13",
-    img: "images/loga/13.webp",
+    img: "/images/loga/13.webp",
   },
   {
     name: "Slide 14",
 
-    img: "images/loga/14.webp",
+    img: "/images/loga/14.webp",
   },
   {
     name: "Slide 15",
-    img: "images/loga/15.webp",
+    img: "/images/loga/15.webp",
   },
   {
     name: "Slide 16",
-    img: "images/loga/16.webp",
+    img: "/images/loga/16.webp",
   },
   {
     name: "Slide 17",
-    img: "images/loga/17.webp",
+    img: "/images/loga/17.webp",
   },
   {
     name: "Slide 18",
-    img: "images/loga/18.webp",
+    img: "/images/loga/18.webp",
   },
   {
     name: "Slide 19",
-    img: "images/loga/19.webp",
+    img: "/images/loga/19.webp",
   },
   {
     name: "Slide 20",
-    img: "images/loga/20.webp",
+    img: "/images/loga/20.webp",
   },
   {
     name: "Slide 20",
-    img: "images/loga/20.webp",
+    img: "/images/loga/20.webp",
   },
   {
     name: "Slide 21",
-    img: "images/loga/21.jpg",
+    img: "/images/loga/21.jpg",
   },
   {
     name: "Slide 22",
-    img: "images/loga/22.jpg",
+    img: "/images/loga/22.jpg",
   },
   {
     name: "Slide 23",
-    img: "images/loga/23.jpg",
+    img: "/images/loga/23.jpg",
   },
   {
     name: "Slide 24",
-    img: "images/loga/24.jpg",
+    img: "/images/loga/24.jpg",
   },
   {
     name: "Slide 25",
-    img: "images/loga/25.jpg",
+    img: "/images/loga/25.jpg",
   },
   {
     name: "Slide 26",
-    img: "images/loga/26.jpg",
+    img: "/images/loga/26.jpg",
   },
   {
     name: "Slide 27",
-    img: "images/loga/27.jpg",
+    img: "/images/loga/27.jpg",
   },
   {
     name: "Slide 28",
-    img: "images/loga/28.jpg",
+    img: "/images/loga/28.jpg",
   },
   {
     name: "Slide 29",
-    img: "images/loga/29.jpg",
+    img: "/images/loga/29.jpg",
   },
   {
     name: "Slide 30",
-    img: "images/loga/30.jpg",
+    img: "/images/loga/30.jpg",
   },
 ];
 
 export const miniaturki = [
   {
     name: "Slide 1",
-    img: "images/miniaturki/zaraz-wracam.webp",
+    img: "/images/miniaturki/zaraz-wracam.webp",
   },
   {
     name: "Slide 2",
-    img: "images/miniaturki/zaraz-zaczynamy.webp",
+    img: "/images/miniaturki/zaraz-zaczynamy.webp",
   },
 ];
 
 export const plakaty = [
   {
     name: "Slide 1",
-    img: "images/plakaty/1.jpg",
+    img: "/images/plakaty/1.jpg",
   },
   {
     name: "Slide 2",
-    img: "images/plakaty/2.jpg",
+    img: "/images/plakaty/2.jpg",
   },
   {
     name: "Slide 3",
-    img: "images/plakaty/3.jpg",
+    img: "/images/plakaty/3.jpg",
   },
   {
     name: "Slide 4",
-    img: "images/plakaty/4.jpg",
+    img: "/images/plakaty/4.jpg",
   },
   {
     name: "Slide 5",
-    img: "images/plakaty/5.jpg",
+    img: "/images/plakaty/5.jpg",
   },
   {
     name: "Slide 6",
-    img: "images/plakaty/6.jpg",
+    img: "/images/plakaty/6.jpg",
   },
   {
     name: "Slide 7",
-    img: "images/plakaty/7.jpg",
+    img: "/images/plakaty/7.jpg",
   },
   {
     name: "Slide 8",
-    img: "images/plakaty/8.jpg",
+    img: "/images/plakaty/8.jpg",
   },
   {
     name: "Slide 9",
-    img: "images/plakaty/9.jpg",
+    img: "/images/plakaty/9.jpg",
   },
   {
     name: "Slide 10",
-    img: "images/plakaty/10.jpg",
+    img: "/images/plakaty/10.jpg",
   },
   {
     name: "Slide 11",
-    img: "images/plakaty/11.jpg",
+    img: "/images/plakaty/11.jpg",
   },
   {
     name: "Slide 12",
-    img: "images/plakaty/12.jpg",
+    img: "/images/plakaty/12.jpg",
   },
   {
     name: "Slide 13",
-    img: "images/plakaty/13.jpg",
+    img: "/images/plakaty/13.jpg",
   },
   {
     name: "Slide 14",
-    img: "images/plakaty/14.jpg",
+    img: "/images/plakaty/14.jpg",
   },
 ];
 
 export const naklejki = [
   {
     name: "Slide 1",
-    img: "images/naklejki/1.jpg",
+    img: "/images/naklejki/1.jpg",
   },
   {
     name: "Slide 2",
-    img: "images/naklejki/2.jpg",
+    img: "/images/naklejki/2.jpg",
   },
   {
     name: "Slide 3",
-    img: "images/naklejki/3.jpg",
+    img: "/images/naklejki/3.jpg",
   },
   {
     name: "Slide 4",
-    img: "images/naklejki/4.jpg",
+    img: "/images/naklejki/4.jpg",
   },
   {
     name: "Slide 5",
-    img: "images/naklejki/5.jpg",
+    img: "/images/naklejki/5.jpg",
   },
   {
     name: "Slide 6",
-    img: "images/naklejki/6.jpg",
+    img: "/images/naklejki/6.jpg",
   },
   {
     name: "Slide 7",
-    img: "images/naklejki/7.jpg",
+    img: "/images/naklejki/7.jpg",
   },
   {
     name: "Slide 8",
-    img: "images/naklejki/8.jpg",
+    img: "/images/naklejki/8.jpg",
   },
   {
     name: "Slide 9",
-    img: "images/naklejki/9.jpg",
+    img: "/images/naklejki/9.jpg",
   },
   {
     name: "Slide 10",
-    img: "images/naklejki/10.jpg",
+    img: "/images/naklejki/10.jpg",
   },
   {
     name: "Slide 11",
-    img: "images/naklejki/11.jpg",
+    img: "/images/naklejki/11.jpg",
   },
   {
     name: "Slide 12",
-    img: "images/naklejki/12.jpg",
+    img: "/images/naklejki/12.jpg",
   },
   {
     name: "Slide 13",
-    img: "images/naklejki/13.jpg",
+    img: "/images/naklejki/13.jpg",
   },
   {
     name: "Slide 14",
-    img: "images/naklejki/14.jpg",
+    img: "/images/naklejki/14.jpg",
   },
 ];
 
 export const ksiazki = [
   {
     name: "Slide 1",
-    img: "images/ksiazki/1.jpg",
+    img: "/images/ksiazki/1.jpg",
   },
   {
     name: "Slide 2",
-    img: "images/ksiazki/2.jpg",
+    img: "/images/ksiazki/2.jpg",
   },
   {
     name: "Slide 3",
-    img: "images/ksiazki/3.jpg",
+    img: "/images/ksiazki/3.jpg",
   },
   {
     name: "Slide 4",
-    img: "images/ksiazki/4.jpg",
+    img: "/images/ksiazki/4.jpg",
   },
   {
     name: "Slide 5",
-    img: "images/ksiazki/5.jpg",
+    img: "/images/ksiazki/5.jpg",
   },
   {
     name: "Slide 6",
-    img: "images/ksiazki/6.jpg",
+    img: "/images/ksiazki/6.jpg",
   },
   {
     name: "Slide 7",
-    img: "images/ksiazki/7.jpg",
+    img: "/images/ksiazki/7.jpg",
   },
   {
     name: "Slide 8",
-    img: "images/ksiazki/8.jpg",
+    img: "/images/ksiazki/8.jpg",
   },
   {
     name: "Slide 9",
-    img: "images/ksiazki/9.jpg",
+    img: "/images/ksiazki/9.jpg",
   },
   {
     name: "Slide 10",
-    img: "images/ksiazki/10.jpg",
+    img: "/images/ksiazki/10.jpg",
   },
   {
     name: "Slide 11",
-    img: "images/ksiazki/11.jpg",
+    img: "/images/ksiazki/11.jpg",
   },
   {
     name: "Slide 12",
-    img: "images/ksiazki/12.jpg",
+    img: "/images/ksiazki/12.jpg",
   },
   {
     name: "Slide 13",
-    img: "images/ksiazki/13.jpg",
+    img: "/images/ksiazki/13.jpg",
   },
   {
     name: "Slide 14",
-    img: "images/ksiazki/14.jpg",
+    img: "/images/ksiazki/14.jpg",
   },
   {
     name: "Slide 15",
-    img: "images/ksiazki/15.jpg",
+    img: "/images/ksiazki/15.jpg",
   },
 ];
